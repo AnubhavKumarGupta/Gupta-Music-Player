eedd2ac52d98aced2a71883f5e0537345f21a038
Anubhav Kumar Gupta <122034618+AnubhavKumarGupta@users.noreply.github.com>
2024-05-12 14:19:46 +0530

    Added Few Songs.
    Added Few Songs and All Set.

--- main.js
+++ main.js
@@ -13,21 +13,30 @@ const nextBtn = $.getElementById("next");
 const background = $.getElementById("background");
 
 const songs = [
-  {
-    path: "https://2022.downloadming.ws/bollywood%20mp3/RRR%20(2022)/02%20-%20Naacho%20Naacho%20(320%20Kbps).mp3",
-    musicName: "Naacho Naacho",
-    artist: "Vishal Mishra, Rahul Sipligunj.",
-    cover:
-      "https://m.media-amazon.com/images/M/MV5BOGEzYzcxYjAtZmZiNi00YzI0LWIyY2YtOTM0MDFjODU2YTZiXkEyXkFqcGdeQXVyMTQ3Mzk2MDg4._V1_.jpg",
-  },
 
   {
-    path: "https://raag.fm/files/mp3/128/Hindi/2308450/Chaar%20Botal%20Vodka%20(Ragini%20MMS%202)%20-%20(Raag.Fm).mp3",
-    musicName: "	Chaar Botal Vodka ",
-    artist: "Yo Yo Honey Singh",
-    cover:
-      "https://resize.indiatvnews.com/en/resize/oldbucket/1200_-/entertainmentbollywood/Ragini-MMS-song12207.jpg",
+    path: "https://pagalfree.com/download/320-Naacho%20Naacho%20-%20RRR%20320%20Kbps.mp3",
+    musicName: "Naacho Naacho",
+    artist: "Vishal Mishra, Rahul Sipligunj.",
+    cover: "https://pagalfree.com/images/128Naacho%20Naacho%20-%20RRR%20128%20Kbps.jpg",
   },
+
+
+  {
+    path: "https://hindi.djpunjab.app/load/K3i7DtkOK2HHkhh39UXW8Q==/Papa%20Meri%20Jaan%20(Childs%20Version).mp3",
+    musicName: "Papa Meri Jaan",
+    artist: "R.P. Krishaang",
+    cover: "https://pagalfree.com/images/128Papa%20Meri%20Jaan%20-%20Animal%20128%20Kbps.jpg",
+  },
+
+
+  {
+    path: "https://pagalfree.com/download/320-Dosti%20-%20RRR%20320%20Kbps.mp3",
+    musicName: "Dosti",
+    artist: "Amit Trivedi, M. M. Keeravani",
+    cover: "https://pagalfree.com/images/128Dosti%20-%20RRR%20128%20Kbps.jpg",
+  },
+
 
   {
     path: "https://2022.downloadming.ws/bollywood%20mp3/Animal%202023/01%20-%20Hua%20Main%20%28320%20Kbps%29.mp3",
@@ -36,28 +45,38 @@ const songs = [
     cover: "https://downloadming.ws/uploads/Animal-2023-MP3-Songs-Download.jpg",
   },
 
+
+
   {
-    path: "https://2022.downloadming.ws/bollywood%20mp3/Tanhaji%20-%20The%20Unsung%20Warrior%20(2020)/Tanhaji%20-%20The%20Unsung%20Warrior%20(2020)%20(320%20Kbps)/03%20-%20Ghamand%20Kar%20-%20DownloadMing.SE.mp3",
-    musicName: "Ghamand Kar",
-    artist: "Parampara Thakur, Sachet Tandon",
-    cover:
-      "https://m.media-amazon.com/images/M/MV5BNTlmMTIxODgtNDFiMS00M2NlLWI4ZDgtYmI4MjhkMjM2ZTBkXkEyXkFqcGdeQXVyMTA5NzIyMDY5._V1_.jpg",
+    path: "https://raag.fm/files/mp3/128/Hindi/2308450/Chaar%20Botal%20Vodka%20(Ragini%20MMS%202)%20-%20(Raag.Fm).mp3",
+    musicName: "Chaar Botal Vodka",
+    artist: "Yo Yo Honey Singh",
+    cover: "https://resize.indiatvnews.com/en/resize/oldbucket/1200_-/entertainmentbollywood/Ragini-MMS-song12207.jpg",
   },
+
+
+  {
+    path: "https://pagalfree.com/download/320-Khairiyat%20(Bonus%20Track)%20-%20Chhichhore%20320%20Kbps.mp3",
+    musicName: "Khairiyat (Bonus Track)",
+    artist: "Arijit Singh",
+    cover: "https://pagalfree.com/images/128Khairiyat%20(Bonus%20Track)%20-%20Chhichhore%20128%20Kbps.jpg",
+  },
+
 
   {
     path: "https://sd2.djjohal.com/128/518405/King%20Shit%20-%20Shubh%20(DJJOhAL.Com).mp3",
     musicName: "King Shit",
     artist: "Shubh",
-    cover:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHNDHlGKABQsZsuJLYehECclROLAhT4pl5oA&s",
+    cover: "https://pagalfree.com/images/128King%20Shit%20-%20Leo%20128%20Kbps.jpg",
   },
+
+
 
   {
     path: "https://new.downloadming.ws/2016a/bollywood%20mp3/K.K.%20Mega%20Hit%20Songs%20Collection%20(2017)/K.K.%20-%20Mega%20Hit%20Songs%20Collection%20(2017)%20(320%20Kbps)/18%20-%20Tujhi%20Mein%20(Crook)%20-%20K.K.%20-%20DownloadMing.SE.mp3",
     musicName: "Tujhi Mein",
     artist: "K.K.",
-    cover:
-      "https://e-cdn-images.dzcdn.net/images/cover/b085365c38eaaa6095ae44723a76f853/500x500-000000-80-0-0.jpg",
+    cover: "https://e-cdn-images.dzcdn.net/images/cover/b085365c38eaaa6095ae44723a76f853/500x500-000000-80-0-0.jpg",
   },
 
 ];
