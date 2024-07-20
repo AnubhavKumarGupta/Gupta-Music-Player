cf1a8c4021fb20ef87343c5f5027b012cbe08d82
Anubhav Kumar Gupta <122034618+AnubhavKumarGupta@users.noreply.github.com>
2024-07-20 15:00:44 +0530

    Added Songs.
    Changed the feature as well as Added Few new Songs.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let $ = document;
 const image = $.getElementById("cover");
 const title = $.getElementById("title");
 const artist = $.getElementById("artist");
-const music = $.querySelector("audio");
+const music = $.getElementById("audio");
 const currentTimeEl = $.getElementById("current-time");
 const durationEl = $.getElementById("duration");
 const progress = $.getElementById("progress");
@@ -15,90 +15,91 @@ const background = $.getElementById("background");
 const songs = [
 
   {
+    path: "https://pagallworld.co.in/wp-content/uploads/2023/11/Cheri-Cheri-Lady.mp3",
+    musicName: "Cheri Cheri Lady.",
+    artist: "Modern Talking.",
+    cover: "https://th.bing.com/th/id/OIP.AtdgLpJxfuYvXSODxdlBxgHaHa?w=171&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+  },
+
+
+  {
     path: "https://pagalfree.com/download/320-Naacho%20Naacho%20-%20RRR%20320%20Kbps.mp3",
-    musicName: "Naacho Naacho",
+    musicName: "Naacho Naacho.",
     artist: "Vishal Mishra, Rahul Sipligunj.",
     cover: "https://pagalfree.com/images/128Naacho%20Naacho%20-%20RRR%20128%20Kbps.jpg",
   },
-
+  
 
   {
     path: "https://www.pagalworld.com.sb/files/download/type/320/id/70532",
-    musicName: "Pehle Bhi Main Remix",
-    artist: "Vishal Mishra",
+    musicName: "Pehle Bhi Main Remix.",
+    artist: "Vishal Mishra.",
     cover: "https://th.bing.com/th/id/OIP.KJiWdwcMZDsU8RgFe1ZzkwHaEK?w=287&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
   },
-
+  
 
   {
-    path: "https://pagalfree.com/download/320-Dosti%20-%20RRR%20320%20Kbps.mp3",
-    musicName: "Dosti",
-    artist: "Amit Trivedi, M. M. Keeravani",
-    cover: "https://pagalfree.com/images/128Dosti%20-%20RRR%20128%20Kbps.jpg",
+    path: "https://www.pagalworld.com.sb/files/download/type/192/id/68445",
+    musicName: "Barbaadiyan Club Mix.",
+    artist: "Sachet Tandon, Nikhita Gandhi.",
+    cover: "https://th.bing.com/th/id/OIP.5f-yxmFE6zfJbbBG3jWZlAHaHa?w=183&h=183&c=7&r=0&o=5&dpr=1.3&pid=1.7",
   },
 
 
   {
-    path: "https://www.mediafire.com/file/cl5u8500s12jx8k/HUA+MAIN+(REMIX)+-+DJ+TARAL.mp3/file",
-    musicName: "Hua Main Remix",
-    artist: "Raghav Chaitanya",
+    path: "https://webrafts.com/wp-content/uploads/music/2024/07/Adele_-_Skyfall_(Jesusfuly.com).mp3",
+    musicName: "Skyfall.",
+    artist: "Adele.",
+    cover: "https://th.bing.com/th/id/OIP.Oa_GV-cVP4ryTL2ITk6kYgHaHa?w=177&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+  },
+
+
+  {
+    path: "https://www.pagalworld.com.sb/files/download/type/320/id/68510",
+    musicName: "Hua Main Remix.",
+    artist: "Raghav Chaitanya.",
     cover: "https://downloadming.ws/uploads/Animal-2023-MP3-Songs-Download.jpg",
   },
-
-
+  
 
   {
-    path: "https://raag.fm/files/mp3/128/Hindi/2308450/Chaar%20Botal%20Vodka%20(Ragini%20MMS%202)%20-%20(Raag.Fm).mp3",
-    musicName: "Chaar Botal Vodka",
-    artist: "Yo Yo Honey Singh",
-    cover: "https://resize.indiatvnews.com/en/resize/oldbucket/1200_-/entertainmentbollywood/Ragini-MMS-song12207.jpg",
+    path: "https://djmaza.live/files/download/type/320/id/14828",
+    musicName: "Love Stereo Again.",
+    artist: "Edward Maya.",
+    cover: "https://www.filmytoday.com/ogimage/news/20131/tiger-shroff-and-zahrah-khan-are-sizzling-in-love-stereo-again/",
   },
 
 
   {
-    path: "https://pagalfree.com/download/320-Khairiyat%20(Bonus%20Track)%20-%20Chhichhore%20320%20Kbps.mp3",
-    musicName: "Khairiyat (Bonus Track)",
-    artist: "Arijit Singh",
-    cover: "https://pagalfree.com/images/128Khairiyat%20(Bonus%20Track)%20-%20Chhichhore%20128%20Kbps.jpg",
+    path: "https://pagal.com.in/assets/images/products/Chhod_Diya_-_Baazaar_128_Kbps.mp3",
+    musicName: "Chhod Diya.",
+    artist: "Arijit Singh.",
+    cover: "https://th.bing.com/th/id/OIP.a7n0hamhjLEPklVFfYYTNwHaKt?w=139&h=200&c=7&r=0&o=5&dpr=1.3&pid=1.7",
   },
 
 
-  {
-    path: "https://sd2.djjohal.com/128/518405/King%20Shit%20-%20Shubh%20(DJJOhAL.Com).mp3",
-    musicName: "King Shit",
-    artist: "Shubh",
-    cover: "https://pagalfree.com/images/128King%20Shit%20-%20Leo%20128%20Kbps.jpg",
-  },
-
-
-
-  {
-    path: "https://new.downloadming.ws/2016a/bollywood%20mp3/K.K.%20Mega%20Hit%20Songs%20Collection%20(2017)/K.K.%20-%20Mega%20Hit%20Songs%20Collection%20(2017)%20(320%20Kbps)/18%20-%20Tujhi%20Mein%20(Crook)%20-%20K.K.%20-%20DownloadMing.SE.mp3",
-    musicName: "Tujhi Mein",
-    artist: "K.K.",
-    cover: "https://e-cdn-images.dzcdn.net/images/cover/b085365c38eaaa6095ae44723a76f853/500x500-000000-80-0-0.jpg",
-  },
 
 ];
 
-let isLoading = false;
+let isPlaying = false;
+let songIndex = 0;
 
 function playSong() {
-  isLoading = true;
+  isPlaying = true;
   playBtn.classList.replace("fa-play", "fa-pause");
   playBtn.setAttribute("title", "Pause");
   music.play();
 }
 
 function pauseSong() {
-  isLoading = false;
+  isPlaying = false;
   playBtn.classList.replace("fa-pause", "fa-play");
   playBtn.setAttribute("title", "Play");
   music.pause();
 }
 
 function playToggle() {
-  if (isLoading) {
+  if (isPlaying) {
     pauseSong();
   } else {
     playSong();
@@ -114,22 +115,10 @@ function loadSongs(song) {
 
 function changeCover(cover) {
   image.classList.remove("active");
-  setTimeout(function () {
+  setTimeout(() => {
     image.src = cover;
     image.classList.add("active");
   }, 100);
-  background.src = cover;
-}
-
-let songIndex = 0;
-
-function prevSong() {
-  songIndex--;
-  if (songIndex < 0) {
-    songIndex = 2;
-  }
-  loadSongs(songs[songIndex]);
-  playSong();
 }
 
 function nextSong() {
@@ -141,28 +130,35 @@ function nextSong() {
   playSong();
 }
 
-loadSongs(songs[songIndex]);
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSongs(songs[songIndex]);
+  playSong();
+}
 
 function updateProgressBar() {
-  if (isLoading) {
+  if (isPlaying) {
     const duration = music.duration;
-    let currentTime = music.currentTime;
-    let progressPercent = (currentTime / duration) * 100;
-    progress.style.width = progressPercent + "%";
+    const currentTime = music.currentTime;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
     const durationMinutes = Math.floor(duration / 60);
-    let durationSeconds = Math.floor(duration % 60);
+    const durationSeconds = Math.floor(duration % 60);
     if (durationSeconds < 10) {
-      durationSeconds = "0" + durationSeconds;
+      durationSeconds = `0${durationSeconds}`;
     }
     if (durationSeconds) {
-      durationEl.innerHTML = durationMinutes + ":" + durationSeconds;
+      durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
     }
     const currentMinutes = Math.floor(currentTime / 60);
     let currentSeconds = Math.floor(currentTime % 60);
     if (currentSeconds < 10) {
-      currentSeconds = "0" + currentSeconds;
+      currentSeconds = `0${currentSeconds}`;
     }
-    currentTimeEl.innerHTML = currentMinutes + ":" + currentSeconds;
+    currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
   }
 }
 
@@ -176,6 +172,8 @@ function setProgressBar(e) {
 playBtn.addEventListener("click", playToggle);
 prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
-music.addEventListener("ended", nextSong);
 music.addEventListener("timeupdate", updateProgressBar);
+music.addEventListener("ended", nextSong);
 progressContainer.addEventListener("click", setProgressBar);
+
+loadSongs(songs[songIndex]);
